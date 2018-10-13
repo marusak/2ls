7b124abe0dcc74df43638077cfa48c38f7d033a3
Matej Marusak <mmarusak@redhat.com>
2018-10-14 00:46:33 +0200

    Move heap 1
    Signed-off-by: Matej Marusak <mmarusak@redhat.com>

--- src/domains/strategy_solver_heap.cpp
+++ src/domains/strategy_solver_heap.cpp
@@ -1,265 +1,156 @@
-/*******************************************************************\
-
-Module: Strategy solver for heap shape analysis
-
-Author: Viktor Malik
-
-\*******************************************************************/
-
-// #define DEBUG_OUTPUT
-
 #include <ssa/ssa_inliner.h>
 #include <algorithm>
 #include "strategy_solver_heap.h"
-
-/*******************************************************************\
-
-Function: strategy_solver_heapt::iterate
-
-  Inputs:
-
- Outputs:
-
- Purpose: Single iteration of invariant inference using heap shape
-          domain.
-
-\*******************************************************************/
 
 bool strategy_solver_heapt::iterate(invariantt &_inv)
 {
   heap_domaint::heap_valuet &inv=static_cast<heap_domaint::heap_valuet &>(_inv);
 
   bool improved=false;
+  heap_domain.pre_iterate_init(_inv);
+  if (heap_domain.something_to_solve()) {
 
-  solver.new_context();
+      solver.new_context();
 
-  // Entry value constraints
-  exprt pre_expr=heap_domain.to_pre_constraints(inv);
-#ifdef DEBUG_OUTPUT
-  debug() << "pre-inv: " << from_expr(ns, "", pre_expr) << eom;
-#endif
-  solver << pre_expr;
+      // Entry value constraints
+      exprt pre_expr=heap_domain.to_pre_constraints(inv);
+      solver << pre_expr;
 
-  // Exit value constraints
-  exprt::operandst strategy_cond_exprs;
-  heap_domain.make_not_post_constraints(
-    inv,
-    strategy_cond_exprs,
-    strategy_value_exprs);
+      // Exit value constraints
+      exprt::operandst strategy_cond_exprs;
+      heap_domain.make_not_post_constraints(
+        inv,
+        strategy_cond_exprs,
+        strategy_value_exprs);
 
-  strategy_cond_literals.resize(strategy_cond_exprs.size());
+      strategy_cond_literals.resize(strategy_cond_exprs.size());
 
-#ifdef DEBUG_OUTPUT
-  debug() << "post-inv: ";
-#endif
-  for(unsigned i=0; i<strategy_cond_exprs.size(); ++i)
-  {
-#ifdef DEBUG_OUTPUT
-    debug() << (i>0 ? " || " : "")
-            << from_expr(ns, "", strategy_cond_exprs[i]);
-#endif
-    strategy_cond_literals[i]=solver.convert(strategy_cond_exprs[i]);
-    strategy_cond_exprs[i]=literal_exprt(strategy_cond_literals[i]);
-  }
-#ifdef DEBUG_OUTPUT
-  debug() << eom;
-#endif
-  solver << disjunction(strategy_cond_exprs);
-
-#ifdef DEBUG_OUTPUT
-  debug() << "solve(): ";
-#endif
-
-  if(solver()==decision_proceduret::D_SATISFIABLE)  // improvement check
-  {
-#ifdef DEBUG_OUTPUT
-    debug() << "SAT" << eom;
-#endif
-
-#ifdef DEBUG_OUTPUT
-    for(unsigned i=0; i<solver.activation_literals.size(); i++)
-    {
-      debug() << "literal: " << solver.activation_literals[i] << " " <<
-              solver.l_get(solver.activation_literals[i]) << eom;
-    }
-    for(unsigned i=0; i<solver.formula.size(); i++)
-    {
-      debug() << "literal: " << solver.formula[i] << " " <<
-              solver.l_get(solver.formula[i]) << eom;
-    }
-    for(unsigned i=0; i<heap_domain.templ.size(); i++)
-    {
-      exprt c=heap_domain.get_row_pre_constraint(i, inv[i]);
-      debug() << "cond: " << from_expr(ns, "", c) << " " <<
-              from_expr(ns, "", solver.get(c)) << eom;
-      debug() << "guards: " << from_expr(ns, "", heap_domain.templ[i].pre_guard)
-              << " "
-              << from_expr(ns, "", solver.get(heap_domain.templ[i].pre_guard))
-              << eom;
-      debug() << "guards: "
-              << from_expr(ns, "", heap_domain.templ[i].post_guard) << " "
-              << from_expr(ns, "", solver.get(heap_domain.templ[i].post_guard))
-              << eom;
-      exprt post=heap_domain.get_row_post_constraint(i, inv[i]);
-      debug() << "post-cond: " << from_expr(ns, "", post) << " "
-              << from_expr(ns, "", solver.get(post)) << eom;
-      print_solver_expr(c);
-      print_solver_expr(post);
-    }
-#endif
-
-    for(unsigned row=0; row<strategy_cond_literals.size(); ++row)
-    {
-      if(solver.l_get(strategy_cond_literals[row]).is_true())
+      for(unsigned i=0; i<strategy_cond_exprs.size(); ++i)
       {
-        debug() << "updating row: " << row << eom;
+        strategy_cond_literals[i]=solver.convert(strategy_cond_exprs[i]);
+        strategy_cond_exprs[i]=literal_exprt(strategy_cond_literals[i]);
+      }
+      solver << disjunction(strategy_cond_exprs);
 
-        const heap_domaint::template_rowt &templ_row=heap_domain.templ[row];
-
-        const exprt loop_guard=to_and_expr(
-          heap_domain.templ[row].pre_guard).op1();
-        find_symbolic_path(loop_guards, loop_guard);
-
-        if(templ_row.expr.id()==ID_and)
+      if(solver()==decision_proceduret::D_SATISFIABLE)  // improvement check
+      {
+        for(unsigned row=0; row<strategy_cond_literals.size(); ++row)
         {
-          // Handle template row with a pair of variables in the expression
-          exprt points_to1=get_points_to_dest(
-            strategy_value_exprs[row].op0(), templ_row.expr.op0());
-          exprt points_to2=get_points_to_dest(
-            strategy_value_exprs[row].op1(), templ_row.expr.op1());
-
-          if(points_to1.is_nil() || points_to2.is_nil())
+          if(solver.l_get(strategy_cond_literals[row]).is_true())
           {
-            if(heap_domain.set_nondet(row, inv))
+
+            const heap_domaint::template_rowt &templ_row=heap_domain.templ[row];
+
+            const exprt loop_guard=to_and_expr(
+              heap_domain.templ[row].pre_guard).op1();
+            find_symbolic_path(loop_guards, loop_guard);
+
+            if(templ_row.expr.id()==ID_and)
             {
-              improved=true;
-              debug() << "Set nondet" << eom;
+              // Handle template row with a pair of variables in the expression
+              exprt points_to1=get_points_to_dest(
+                strategy_value_exprs[row].op0(), templ_row.expr.op0());
+              exprt points_to2=get_points_to_dest(
+                strategy_value_exprs[row].op1(), templ_row.expr.op1());
+
+              if(points_to1.is_nil() || points_to2.is_nil())
+              {
+                if(heap_domain.set_nondet(row, inv))
+                {
+                  improved=true;
+                }
+              }
+              else
+              {
+                if(heap_domain.add_points_to(
+                  row, inv, and_exprt(points_to1, points_to2)))
+                {
+                  improved=true;
+                  const std::string info=
+                    templ_row.mem_kind==heap_domaint::STACK ? "points to "
+                                                            : "path to ";
+                }
+              }
+              continue;
+            }
+
+            int actual_loc=heap_domain.get_symbol_loc(templ_row.expr);
+
+            exprt points_to=get_points_to_dest(
+              strategy_value_exprs[row], templ_row.expr);
+
+            if(points_to.is_nil())
+            {
+              if(heap_domain.set_nondet(row, inv))
+              {
+                improved=true;
+              }
+              continue;
+            }
+            else
+            {
+              if(heap_domain.add_points_to(row, inv, points_to))
+              {
+                improved=true;
+                const std::string info=
+                  templ_row.mem_kind==heap_domaint::STACK ? "points to "
+                                                          : "path to ";
+              }
+            }
+
+            // If the template row is of heap kind, we need to ensure the
+            // transitive closure over the set of all paths
+            if(templ_row.mem_kind==heap_domaint::HEAP &&
+               points_to.type().get_bool("#dynamic") &&
+               points_to.id()==ID_symbol &&
+               id2string(to_symbol_expr(points_to).get_identifier()).find(
+                 "$unknown")==
+               std::string::npos)
+            {
+              // Find row with corresponding member field of the pointed object
+              // (obj.member)
+              int member_val_index;
+              member_val_index=
+                find_member_row(
+                  points_to,
+                  templ_row.member,
+                  actual_loc,
+                  templ_row.kind);
+              if(member_val_index>=0 && !inv[member_val_index].nondet)
+              {
+                // Add all paths from obj.next to p
+                if(heap_domain.add_transitivity(
+                  row,
+                  static_cast<unsigned>(member_val_index),
+                  inv))
+                {
+                  improved=true;
+                  const std::string expr_str=
+                    from_expr(ns, "", heap_domain.templ[member_val_index].expr);
+                }
+              }
+            }
+
+            // Recursively update all rows that are dependent on this row
+            if(templ_row.mem_kind==heap_domaint::HEAP)
+            {
+              updated_rows.clear();
+              if(!inv[row].nondet)
+                update_rows_rec(row, inv);
+              else
+                clear_pointing_rows(row, inv);
             }
           }
-          else
-          {
-            if(heap_domain.add_points_to(
-              row, inv, and_exprt(points_to1, points_to2)))
-            {
-              improved=true;
-              const std::string info=
-                templ_row.mem_kind==heap_domaint::STACK ? "points to "
-                                                        : "path to ";
-              debug() << "Add " << info
-                      << from_expr(ns, "", and_exprt(points_to1, points_to2))
-                      << eom;
-            }
-          }
-          continue;
-        }
-
-        int actual_loc=heap_domain.get_symbol_loc(templ_row.expr);
-
-        exprt points_to=get_points_to_dest(
-          strategy_value_exprs[row], templ_row.expr);
-
-        if(points_to.is_nil())
-        {
-          if(heap_domain.set_nondet(row, inv))
-          {
-            improved=true;
-            debug() << "Set nondet" << eom;
-          }
-          continue;
-        }
-        else
-        {
-          if(heap_domain.add_points_to(row, inv, points_to))
-          {
-            improved=true;
-            const std::string info=
-              templ_row.mem_kind==heap_domaint::STACK ? "points to "
-                                                      : "path to ";
-            debug() << "Add " << info << from_expr(ns, "", points_to) << eom;
-          }
-        }
-
-        // If the template row is of heap kind, we need to ensure the
-        // transitive closure over the set of all paths
-        if(templ_row.mem_kind==heap_domaint::HEAP &&
-           points_to.type().get_bool("#dynamic") &&
-           points_to.id()==ID_symbol &&
-           id2string(to_symbol_expr(points_to).get_identifier()).find(
-             "$unknown")==
-           std::string::npos)
-        {
-          // Find row with corresponding member field of the pointed object
-          // (obj.member)
-          int member_val_index;
-          member_val_index=
-            find_member_row(
-              points_to,
-              templ_row.member,
-              actual_loc,
-              templ_row.kind);
-          if(member_val_index>=0 && !inv[member_val_index].nondet)
-          {
-            // Add all paths from obj.next to p
-            if(heap_domain.add_transitivity(
-              row,
-              static_cast<unsigned>(member_val_index),
-              inv))
-            {
-              improved=true;
-              const std::string expr_str=
-                from_expr(ns, "", heap_domain.templ[member_val_index].expr);
-              debug() << "Add all paths: " << expr_str
-                      << ", through: " << from_expr(ns, "", points_to) << eom;
-            }
-          }
-        }
-
-        // Recursively update all rows that are dependent on this row
-        if(templ_row.mem_kind==heap_domaint::HEAP)
-        {
-          updated_rows.clear();
-          if(!inv[row].nondet)
-            update_rows_rec(row, inv);
-          else
-            clear_pointing_rows(row, inv);
         }
       }
-    }
-  }
 
-  else
-  {
-    debug() << "UNSAT" << eom;
-
-#ifdef DEBUG_OUTPUT
-    for(unsigned i=0; i<solver.formula.size(); i++)
-    {
-      if(solver.solver->is_in_conflict(solver.formula[i]))
-        debug() << "is_in_conflict: " << solver.formula[i] << eom;
       else
-        debug() << "not_in_conflict: " << solver.formula[i] << eom;
-    }
+      {
+        debug() << "UNSAT" << eom;
 
-    for(unsigned i=0; i<heap_domain.templ.size(); i++)
-    {
-      exprt c=heap_domain.get_row_pre_constraint(i, inv[i]);
-      debug() << "cond: " << from_expr(ns, "", c) << " " <<
-              from_expr(ns, "", solver.get(c)) << eom;
-      debug() << "guards: " << from_expr(ns, "", heap_domain.templ[i].pre_guard)
-              << " "
-              << from_expr(ns, "", solver.get(heap_domain.templ[i].pre_guard))
-              << eom;
-      debug() << "guards: "
-              << from_expr(ns, "", heap_domain.templ[i].post_guard) << " "
-              << from_expr(ns, "", solver.get(heap_domain.templ[i].post_guard))
-              << eom;
-      exprt post=heap_domain.get_row_post_constraint(i, inv[i]);
-      debug() << "post-cond: " << from_expr(ns, "", post) << " "
-              << from_expr(ns, "", solver.get(post)) << eom;
-    }
-#endif
+      }
+      solver.pop_context();
   }
-  solver.pop_context();
 
   return improved;
 }
